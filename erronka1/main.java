c59585011f10655d0d1dc4807f73684f45ebc706
Andonigt04 <98283996+Andonigt04@users.noreply.github.com>
2023-11-02 18:03:57 +0100

    Add files via upload

---
+++ erronka1/main.java
@@ -0,0 +1,46 @@
+package erronka1;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class main extends erronka1.signUp{
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					main window = new main();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public main() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 800, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		frame.getContentPane().add(signUpPane);
+		
+	}
+
+}
---
+++ erronka1/signIn.java
@@ -0,0 +1,49 @@
+package erronka1;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class signIn {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					signIn window = new signIn();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public signIn() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 800, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		signInPane.setBounds(100, 100, 800, 500);
+		frame.getContentPane().add(signInPane);
+	}
+	
+	public JPanel signInPane = new JPanel();
+
+}
